6825e4ae6cb8245110b890869c8bd0bee4b191ef
inmajhossain <inmaj670@gmail.com>
2024-10-09 14:39:17 +0600

    new correction

---
+++ 21 Nexux/app.js
@@ -0,0 +1,9 @@
+// Add event listener to each dropdown menu
+function toggleDropdown() {
+  var dropdownMenu = document.querySelector('.dropdown-menu');
+  dropdownMenu.classList.toggle('hidden');
+}
+
+
+
+// For Swiper
